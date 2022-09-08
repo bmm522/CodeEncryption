8eba4e617ea877da07fd31d2de61dd3d6edcadd3
JiIn Kim <rlawldls11@naver.com>
2022-09-08 20:51:24 +0900

    test성공

---
+++ src/test/run/Run.java
@@ -0,0 +1,18 @@
+package test.run;
+
+import java.util.ArrayList;
+
+import codeEncryption.oneWay.codeObject.CodeObject;
+import codeEncryption.oneWay.main.CodeEncryptionOfOneWay;
+
+public class Run {
+	public static void main(String[] args) {
+		String id = "bmm522";
+		String pwd = "1234";
+		ArrayList<CodeObject> arr = new ArrayList<CodeObject>();
+		CodeEncryptionOfOneWay cw = new CodeEncryptionOfOneWay(id, pwd);
+		arr = cw.getEncryptingCode();
+		
+		System.out.println(arr.get(0).getIdCode()+", "+ arr.get(0).getSaltCode()+", "+ arr.get(0).getHashCode());
+	}
+}
