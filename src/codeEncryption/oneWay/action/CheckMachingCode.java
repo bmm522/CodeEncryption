a7f192374421ae5cdd9d9f067a702c9ed274ca59
JiIn Kim <rlawldls11@naver.com>
2022-09-13 16:26:01 +0900

    구조 변경

--- src/codeEncryption/oneWay/action/CheckMachingCode.java
+++ src/codeEncryption/oneWay/action/CheckMachingCode.java
@@ -1,20 +1,12 @@
 package codeEncryption.oneWay.action;
 
 import codeEncryption.oneWay.action.hash.HashCode;
-import codeEncryption.oneWay.action.salt.Salt;
-import codeEncryption.oneWay.codeObject.CodeEntity;
 
 public class CheckMachingCode {
-	private CodeEntity cObj = new CodeEntity();
-	private Salt ms = new Salt();
+//	private CodeEntity cObj = new CodeEntity();
+//	private Salt ms = new Salt();
 	private HashCode hc = new HashCode();
 	
-	public CodeEntity actionOfEncryptingCode(String idCode, String pwdCode) {
-		cObj.setIdCode(idCode);
-		cObj.setSaltCode(ms.actionOfMakeSalt());
-		cObj.setHashCode(hc.actionOfMakeHash(ms.getSalt(), pwdCode));
-		return cObj;
-	}
 	
 	public boolean checkMachingCode(String idCode, String pwdCode, String salt, String hashCode) {
 		return hc.checkMachingHashCode(idCode, pwdCode, salt, hashCode);
--- src/codeEncryption/oneWay/main/CodeEncryptionOfOneWay.java
+++ src/codeEncryption/oneWay/main/CodeEncryptionOfOneWay.java
@@ -3,12 +3,18 @@ package codeEncryption.oneWay.main;
 import java.util.ArrayList;
 
 import codeEncryption.oneWay.action.CheckMachingCode;
+import codeEncryption.oneWay.action.hash.HashCode;
+import codeEncryption.oneWay.action.salt.Salt;
 import codeEncryption.oneWay.codeObject.CodeEntity;
 
 public class CodeEncryptionOfOneWay {
 	private String idCode;
 	private String pwdCode;
+	private CodeEntity cObj = new CodeEntity();
+	private Salt ms = new Salt();
+	private HashCode hc = new HashCode();
 	private CheckMachingCode ma = new CheckMachingCode();
+	
 	public CodeEncryptionOfOneWay(String idCode, String pwdCode) {
 		this.idCode = idCode;
 		this.pwdCode = pwdCode;
@@ -19,9 +25,16 @@ public class CodeEncryptionOfOneWay {
 	}
 
 	private ArrayList<CodeEntity> getArrayListOfCodeEncryptionOfOneWay() {
-		ArrayList<CodeEntity> co = new ArrayList<CodeEntity>();
-		co.add(ma.actionOfEncryptingCode(idCode, pwdCode));
-		return co;
+		ArrayList<CodeEntity> ce = new ArrayList<CodeEntity>();
+		ce.add(actionOfEncryptingCode(idCode, pwdCode));
+		return ce;
+	}
+	
+	public CodeEntity actionOfEncryptingCode(String idCode, String pwdCode) {
+		cObj.setIdCode(idCode);
+		cObj.setSaltCode(ms.actionOfMakeSalt());
+		cObj.setHashCode(hc.actionOfMakeHash(ms.getSalt(), pwdCode));
+		return cObj;
 	}
 	
 	
