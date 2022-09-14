b60709f91944194c5cad1bef2abad73607ca4c15
JiIn Kim <rlawldls11@naver.com>
2022-09-14 15:20:53 +0900

    이름 오타수정

--- src/codeEncryption/oneWay/action/check/CheckMatchingCode.java
+++ src/codeEncryption/oneWay/action/check/CheckMatchingCode.java
@@ -2,17 +2,17 @@ package codeEncryption.oneWay.action.check;
 
 import codeEncryption.oneWay.action.hash.HashCode;
 
-public class CheckMachingCode {
+public class CheckMatchingCode {
 //	private CodeEntity cObj = new CodeEntity();
 //	private Salt ms = new Salt();
 	private HashCode hc = new HashCode();
 	
 	
-	public boolean checkMachingCode(String pwdCode, String salt, String hashCode) {
-		return checkMachingHashCode(pwdCode, salt, hashCode);
+	public boolean checkMatchingCode(String pwdCode, String salt, String hashCode) {
+		return checkMatchingHashCode(pwdCode, salt, hashCode);
 	}
 	
-	private boolean checkMachingHashCode(String pwdCode, String salt, String hashCode) {
+	private boolean checkMatchingHashCode(String pwdCode, String salt, String hashCode) {
 		if(hc.actionOfEncryption(hc.plusStr(salt, pwdCode)).equals(hashCode)){
 			return true;
 		}
