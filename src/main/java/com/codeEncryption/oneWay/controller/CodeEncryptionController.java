9a2fed1dabe3de6ca933e2129136e797fbbbe10f
JiIn Kim <rlawldls11@naver.com>
2022-10-15 16:54:08 +0900

    test : 통신 test

--- src/main/java/com/codeEncryption/oneWay/controller/CodeEncryptionController.java
+++ src/main/java/com/codeEncryption/oneWay/controller/CodeEncryptionController.java
@@ -1,10 +1,15 @@
 package com.codeEncryption.oneWay.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CodeEncryptionController {
 	
+	@PostMapping("/code-encryption")
+	public void actionTest() {
+		System.out.println("통신성공");
+	}
 
 
 }	
